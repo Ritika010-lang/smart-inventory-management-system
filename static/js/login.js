bd700d6c748e05edad44cce155f27f08e7fa1af6
hk123 <drhk2005@gmail.com>
2026-07-07 15:40:13 +0530

    Added professional login page UI

---
+++ static/js/login.js
@@ -0,0 +1,26 @@
+const password = document.getElementById("password");
+const togglePassword = document.getElementById("togglePassword");
+
+togglePassword.addEventListener("click", () => {
+
+    if(password.type==="password"){
+
+        password.type="text";
+
+        togglePassword.classList.remove("fa-eye");
+
+        togglePassword.classList.add("fa-eye-slash");
+
+    }
+
+    else{
+
+        password.type="password";
+
+        togglePassword.classList.remove("fa-eye-slash");
+
+        togglePassword.classList.add("fa-eye");
+
+    }
+
+});
